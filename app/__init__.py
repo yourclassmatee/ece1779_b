c930da21d4381b5c32e70f1dc1f38f4604fc5295
max <maxsheng101@gmail.com>
2018-11-16 11:54:39 -0500

    no export

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 config_name = os.getenv('FLASK_CONFIG')
 webapp = Flask(__name__, instance_relative_config=True)
-webapp.config.from_object(app_config[config_name])
+#webapp.config.from_object(app_config[config_name])
 webapp.config.from_pyfile('config.py')
 
 from app import main
--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 #!venv/bin/python
 from app import webapp
 if __name__ == '__main__':
-  webapp.run(host='0.0.0.0', debug=True)
+  webapp.run(host='0.0.0.0', debug=False)
 
